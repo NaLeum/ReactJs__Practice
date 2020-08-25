e31fe6d957015ee056861efe9d3119c2115f447e
NaLeum <wognsdl@likelion.org>
2020-08-25 14:40:57 +0900

    EventPreactice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MyComponent from './MyComponent'
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice';
+import EventPracticeF from './EventPracticeF'
 
 class App extends Component {
   render(){
@@ -12,6 +13,7 @@ class App extends Component {
   <Counter/>
   <Say/>
   <EventPractice />
+  <EventPracticeF />
   </>
   )
   }
