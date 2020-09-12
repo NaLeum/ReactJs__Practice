c51fbe13b4e396124e4ac68ca87248008c6d2b47
NaLeum <wognsdl@likelion.org>
2020-09-12 22:36:18 +0900

    기본 나누기, useState

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Counter from './Counter';
+import Info from './Info';
+
+const App= () => {
+  return (
+    <>
+     <Counter />
+     <Info />
+    </>
+  );
+}
+
+export default App;
