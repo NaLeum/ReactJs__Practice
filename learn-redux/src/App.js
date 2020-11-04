44c4ce897fdd5507d1710445eefef5f5a3c62bc7
NaLeum <wognsdl@likelion.org>
2020-11-04 17:16:43 +0900

    todo 최적화

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import TodoContainer from './containers/TodosContainer';
 
 function App() {
   return (
+    <>
     <CounterContainer />
+    <hr />
+    <TodoContainer />
+    </>
   );
 }
 
--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import Counter from '../components/Counter';
 // 상태 조회
-import { useDispatch, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { decrease, increase, setDiff } from '../modules/counter';
 
 const CounterContainer = () => {
     
     // 상태 조회
+    // const { number, diff } = useSelector(state => ({
+    //     number: state.counter.number,
+    //     diff: state.counter.diff
+    // }));
+
+    // 최적화 1번
+    // const number = useSelector(state => state.counter.number);
+    // const diff = useSelector(state => state.counter.diff);
+
+    // 최적화 2번 (이전상태값과 비교 )
     const { number, diff } = useSelector(state => ({
         number: state.counter.number,
         diff: state.counter.diff
-    }));
-
+    }),
+    shallowEqual
+    );
     const dispatch = useDispatch();
 
     const onIncrease = () => dispatch(increase());
